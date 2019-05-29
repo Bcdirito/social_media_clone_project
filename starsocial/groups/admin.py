18c3c7a225ac0ca932c7088d04c6dda7d8755905
Brian DiRito <bdirito91@gmail.com>
2019-05-29 15:13:03 -0400

    added group urls and registered group models

--- starsocial/groups/admin.py
+++ starsocial/groups/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import models
 
 # Register your models here.
+class GroupMemberInLine(admin.TabularInline):
+    model = models.GroupMember
+    
+admin.site.register(models.Group)
