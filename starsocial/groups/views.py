58b6f368a84b5d4fd61cef6329b7607a4927ead1
Brian DiRito <bdirito91@gmail.com>
2019-05-29 16:36:37 -0400

    rough draft of project completed

--- starsocial/groups/views.py
+++ starsocial/groups/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.urlresolvers import reverse
 from django.views import generic
+from django.contrib import messages
 
 from .models import Group, GroupMember
 
@@ -16,3 +17,33 @@ class SingleGroup(generic.DetailView):
 
 class ListGroups(generic.ListView):
     model = Group
+
+class JoinGroup(LoginRequiredMixin, generic.RedirectView):
+    def get_redirect_url(self, *args, **kwargs):
+        return reverse("groups:single", kwargs={"slug":self.kwargs.get("slug")})
+
+    def get(self, request, *args, **kwargs):
+        group = get_object_or_404(Group, slug=self.kwargs.get("slug"))
+
+        try:
+            GroupMember.objects.create(user=self.request.user, group=group)
+        except IntegrityError:
+            messages.warning(self.request=("Already a member!"))
+        else:
+            messages.success(self.request, "You are now a member!")
+
+        return super().get(request, *args, **kwargs)
+
+class LeaveGroup(LoginRequiredMixin, generic.RedirectView):
+    def get_redirect_url(self, *args, **kwargs):
+        return reverse("groups:single", kwargs={"slug":self.kwargs.get("slug")})
+
+    def get(self, request, *args, **kwargs):
+        try:
+            membership = models.GroupMember.objects.filter(user=self.request.user, group_slug=self.kwargs.get("slug")).get()
+        except models.GroupMember.DoesNotExist:
+            messages.warning(self.request, "Sorry you are not in this group!")
+        else:
+            membership.delete()
+            messages.success(self.request, "You have left the group!")
+        return super().get(request, *args, **kwargs)
