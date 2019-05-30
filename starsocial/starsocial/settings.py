c214e0b5c49783f218fb906993d933eb9b833b88
Brian DiRito <bdirito91@gmail.com>
2019-05-30 00:17:32 -0400

    added debug toolbar

--- starsocial/starsocial/settings.py
+++ starsocial/starsocial/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'bootstrap3',
     'accounts',
     'groups',
-    'posts'
+    'posts',
+    'debug_toolbar'
 ]
 
 MIDDLEWARE = [
@@ -51,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'starsocial.urls'
@@ -126,3 +128,5 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 LOGIN_REDIRECT_URL = "test"
 LOGOUT_REDIRECT_URL = "thanks"
+
+INTERNAL_IPS = ["127.0.0.1"]
